92ff918155c96bee00a4cf446cda1a01998a3fd3
yu imaoka <d_tecnolife1009@icloud.com>
2020-02-19 16:43:01 +0000

    fav btn

--- app/javascript/components/favorite/favorite_btn.vue
+++ app/javascript/components/favorite/favorite_btn.vue
@@ -3,7 +3,7 @@
     <div v-if="isfavorited" @click="deletefavorite()">
       <img src=".images/heart-red.png" alt="いいねボタン"> {{ count }}
     </div>
-    <div v-else @click="registerLike()">
+    <div v-else @click="registerfavorite()">
       <img src=".images/heart-black.png" alt="いいねボタン"> {{ count }}
     </div>
   </div>
@@ -11,12 +11,59 @@
 
 
 <script>
+import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken()
+
 export default {
-  data: function () {
+  props: ['userId', 'postId'],
+  data: function() {
     return {
-      message: "Hello Vue!"
+      favoriteList: []
     }
+  },
+  computed: {
+    count() {
+      return this.favoriteList.length;
+    },
+    isfavorited() {
+      if (this.favoriteList.length === 0 ) {return false}
+      return Boolean(this.findFavoriteId())
+    }
+  },
+  created: function() {
+    this.feathFavoriteByPostId().then(result => {
+      this.favoriteList = result
+    })
+  },
+  
+  method: {
+      feathFavoriteByPostId: async function() {
+        const res = await axios.get(`api/favorites/?post_id=${this.postId}`);
+        if (res.status !== 200){process.exit()};
+        return res.data
+      },
+      registerfavorite: async function() {
+        const res = await axios.post('api/favorites', {post_id: this.postId})
+        if (res.status !== 201) {process.exit()}
+        this.feathFavoriteByPostId().then(result =>{
+          this.favoriteList = result
+        })
+      },
+      deletefavorite: async function(){
+        const favId = this.findFavoriteId()
+        const res = await axios.delete(`api/favorites/${favId}`)
+        if (res.status !== 200) {process.exit()}
+        this.favoriteList = this.favoriteList.filter(n => n.id !== favId)
+      },
+      findFavoriteId: function(){
+        const fav = this.favoriteList.find((fav) => {
+          return fav.user_id === this.userId
+        })
+        if (fav) {return fav.id};
+      }
   }
+  
 }
 </script>
 
